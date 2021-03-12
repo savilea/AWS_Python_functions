9f5cefab59640b6138415974568404fe64410bae
savilea <adamsaville7@hotmail.co.uk>
2021-03-12 10:33:59 +0000

    Create read_file_from_s3.py
    Reads a file stored on S3 and loads it into a dataframe

---
+++ read_file_from_s3.py
@@ -0,0 +1,18 @@
+import boto3, awswrangler as wr
+
+def read_file_from_s3(s3_path):
+    file_type = s3_path.split(".")[1:]
+    s3_df=[]
+    if file_type==['json']:
+        s3_df = wr.s3.read_json(path=s3_path)
+        print(s3_df)
+    elif file_type==['csv']:
+        s3_df = wr.s3.read_csv(path=s3_path)
+        print(s3_df)
+    elif file_type==['parquet']:
+        s3_df = wr.s3.read_parquet(path=s3_path)
+        print(s3_df)
+    else:
+        print('Unexpected file type')
+    return s3_df
+
